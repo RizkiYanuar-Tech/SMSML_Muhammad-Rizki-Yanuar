d2be3c26764a5136be1dedccc7c36062405c5fe2
Rizki Yanuar <ryanuar83@gmail.com>
2025-09-08 21:11:55 +0700

    Update file modelling.py

--- Workflow_CI/MLProjects/modelling.py
+++ Workflow_CI/MLProjects/modelling.py
@@ -12,20 +12,26 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 #Get root project
 project_root = os.path.dirname(current_dir)
 
+repo_root = os.path.dirname(project_root)
+
 if project_root not in sys.path:
     sys.path.append(project_root)
     print(f"Project root ditambahkan ke path: {project_root}")
 
+if repo_root not in sys.path:
+    sys.path.append(repo_root)
+
 from Preprocessing.Automate_MuhammadRizkiYanuar import preprocess
 
 #LOAD DATA
-data_path = os.path.join(project_root, 'StudentPerformanceFactors.csv')
+data_path = os.path.join(repo_root, 'StudentPerformanceFactors.csv')
 
 try:
     stuper = pd.read_csv(data_path)
     print(f"\nDataset berhasil dimuat dari: {data_path}")
 except FileNotFoundError:
     print(f"file tidak ditemukan di {data_path}")
+    sys.exit(1)
 
 #Preprocessing & Splitting Data
 X_train, X_test, y_train, y_test = preprocess(stuper, 'Exam_Score', ['Hours_Studied','Tutoring_Sessions'], 'Preprocess_pipeline.joblib', 'Train_studentperformancefactors.csv', 'Test_studentperformancefactors.csv')

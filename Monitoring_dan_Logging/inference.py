9cae40e191ba814f6a966fbc45f20a2b4946a1b4
Rizki Yanuar <ryanuar83@gmail.com>
2025-09-09 19:50:55 +0700

    Update inference.py

--- Monitoring_dan_Logging/inference.py
+++ Monitoring_dan_Logging/inference.py
@@ -1,11 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Histogram
 from typing import List, Any
 import mlflow
 import sys
 import os
 import pandas as pd
+
+#Customize Metrik
+PREDICTION_DISTRIBUTION = Histogram(
+    'model_prediction_score', #Title
+    'Prediction_score_distribution_Model', #Description
+    buckets=(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+)
 
 #Load Model
 MODEL_RUN_ID = os.environ.get("MODEL_RUN_ID")
@@ -17,7 +25,7 @@ if not MODEL_RUN_ID:
 model_uri = f"runs:/{MODEL_RUN_ID}/model"
 
 #Search mlruns folder
-os.environ["MLflow_tracking_uri"] = "file:./Workflow_CI/MLProjects/mlruns"
+os.environ["MLFLOW_TRACKING_URI"] = "file:./mlruns"
 
 print(f"Memuat model dari URI: {model_uri}")
 model = mlflow.pyfunc.load_model(model_uri)
@@ -40,6 +48,19 @@ async def predict(input_data: PredictionInput):
     try:
         df = pd.DataFrame(input_data.dataframe_split.data, columns=input_data.dataframe_split.columns)
         predictions = model.predict(df)
-        return {"predictions": predictions.tolist()}
+        prediction_list = predictions.tolist()
+        
+        #Observe Customs Metric
+        try:
+            for pred_value in prediction_list:
+                PREDICTION_DISTRIBUTION.observe(int(pred_value))
+        except Exception as e:
+            print(f"Gagal mengamati metrik: {e}")
+            return {"predictions": prediction_list}
+        
+        #Success output
+        return {"predictions": prediction_list} 
+        
     except Exception as e:
-        return {"error": str(e)}, 500
+        print(f"Error: {e}")
+        raise HTTPException(status_code=500, detail=f'Internal Server Error: {str(e)}')
